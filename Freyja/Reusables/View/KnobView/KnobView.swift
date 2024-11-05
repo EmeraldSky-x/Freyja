024d2bd5e0087f6f011aad9e5fbc48f257894191
Swathy Sudarsanan <>
2024-11-05 08:07:09 +0530

    Basic conversation added

--- Freyja/Reusables/View/KnobView/KnobView.swift
+++ Freyja/Reusables/View/KnobView/KnobView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import CoreGraphics
+import Combine
 
 class KnobView: UIView {
 //MARK: - Decalrations: Logic drivers
@@ -237,11 +238,18 @@ class KnobView: UIView {
     }()
     lazy var screenText: UILabel = {
         let label = UILabel()
-        label.text = "Knob"
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = UIColor.black.withAlphaComponent(0.4)
+        label.font = UIFont(name: "alienleaguebold", size: 14)
+        label.numberOfLines = -1
+        label.textAlignment = .center
         return label
     }()
+    var conversationManager: ConversationManager? = {
+        let manager = ConversationManager()
+        return manager
+    }()
+    var cancellableSet: Set<AnyCancellable> = .init()
+    var shouldInitiateViews = true
 //MARK: - Initiation
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -252,9 +260,18 @@ class KnobView: UIView {
     }
     override func layoutSubviews() {
         super.layoutSubviews()
-        initViews()
-        self.addGestureRecognizer(panGesture)
-        screenView.addGestureRecognizer(tapGesture)
+        if shouldInitiateViews {
+            screenText.text = ConversationMessages.instructions.rawValue
+            initViews()
+            self.addGestureRecognizer(panGesture)
+            screenView.addGestureRecognizer(tapGesture)
+            conversationManager?.currentMessagePublisher.sink { [weak self] message in
+                print(message.rawValue)
+                self?.screenText.text = message.rawValue
+            }.store(in: &cancellableSet)
+            conversationManager?.appOpened()
+        }
+        shouldInitiateViews = false
     }
 //MARK: - Selectors for Gestures
     @objc func screenTapped(sender: UITapGestureRecognizer) {
